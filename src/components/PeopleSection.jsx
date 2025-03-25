795b2b426df62ca91cd3184f645b45d00b90c4bb
Tania Choque <taniachque@gmail.com>
2025-03-25 00:38:06 -0400

    Adding peoplesection in index.

---
+++ src/components/PeopleSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const PeopleSection = () => {
+return (
+<section id="people-images" className="w-screen">
+<img
+src="/assets/images/personajessimi.png"
+className="w-screen h-auto object-cover"
+alt="Personajes Simi"
+/>
+</section>
+);
+};
+
+export default PeopleSection;
