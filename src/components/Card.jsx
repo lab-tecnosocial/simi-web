8c007ab1f65ca1e224259e34323c22dffa0bf3d1
Tania Choque <taniachque@gmail.com>
2025-03-25 02:20:09 -0400

    Refactor navbar,peoplesection,team.

---
+++ src/components/Card.jsx
@@ -0,0 +1,24 @@
+// Card.jsx
+import React from 'react';
+
+const Card = ({ nombre, imagen, linkedin, profesion }) => {
+  return (
+    <div className="bg-white rounded-lg overflow-hidden flex flex-col items-center m-2">
+      {/* Imagen circular y centrada */}
+      <img
+        className="w-24 h-24 object-cover rounded-full mt-4" 
+        src={imagen}
+        alt={nombre}
+      />
+      <div className="p-4 text-center">
+        <h2 className="text-xl font-semibold">{nombre}</h2>
+        <p className="text-gray-600">{profesion}</p> {/* Mostrar la profesión */}
+        <a href={linkedin} target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-linkedin w-6 h-6 mt-2 text-blue-700" aria-hidden="true"></i>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
