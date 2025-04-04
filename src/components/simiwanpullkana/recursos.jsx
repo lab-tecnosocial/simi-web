89c053d486c7da450de2dbfb13a06bfd8ccf4e1a
CtrlRachiee <rqlcal@gmail.com>
2025-04-03 21:57:46 -0400

    Hero modified at Simiwan Pullkana page, added footer and header too.

--- src/components/simiwanpullkana/recursos.jsx
+++ src/components/simiwanpullkana/recursos.jsx
@@ -27,33 +27,39 @@ const Recursos = () => {
         },
         { 
             imagen: '/assets/images/juego/recursos/anexo.png',
-            descripcion: 'Descarga el anexo extras(material para maestrxs)',
+            descripcion: 'Descarga el anexo extras (material para maestrxs)',
             link:'https://drive.google.com/file/d/11q2UQui3h6La-0eBtRfvu98HkCCZ9gtD/view?usp=sharing'
         }
     ];
 
     return (
-        <section className="py-2 container mx-auto">
-            <div className="grid grid-cols-2 md:grid-cols-2 gap-4">
-                {recursos.map((recurso, index) => (
-                    <div 
-                        key={index} 
-                        className="flex flex-col justify-center items-center p-2"
-                    >
-                        <a href={recurso.link} target="_blank" rel="noopener noreferrer" download>
-                            <img 
-                                src={recurso.imagen} 
-                                alt={recurso.descripcion}
-                                className="max-h-48 max-w-full object-contain mb-1"
-                            />
-                        </a>
-                        <p className="text-center text-sm text-gray-600">
-                            {recurso.descripcion}
-                        </p>
-                    </div>
-                ))}
+        <section className="py-0 px-0 container">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-4 ml-[10%] sm:ml-[5%]">
+        {recursos.map((recurso, index) => (
+            <div 
+                key={index} 
+                className="flex flex-col justify-start items-center p-4"
+            >
+                <a href={recurso.link} target="_blank" rel="noopener noreferrer" download>
+                    <img 
+                        src={recurso.imagen} 
+                        alt={recurso.descripcion}
+                        className="h-[230px] max-w-[230px] object-contain mb-[15px] mt-[20px] ml-[5px] mr-[5px]"
+                    />
+                </a>
+                <p className="text-center text-[18px] text-gray-600 font-[Text Me One] max-w-[250px] break-words">
+                    {recurso.descripcion}
+                </p>
             </div>
-        </section>
+        ))}
+    </div>
+</section>
+
+
+
+
+
+
     );
 };
 
