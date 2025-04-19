0e0f1b066bb270161801114b2c813fecfa2d486f
Tania Choque <taniachque@gmail.com>
2025-04-19 10:38:08 -0400

    Updated anagram game without timer.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,30 @@ export default {
     "./src/**/*.{astro,js,jsx,ts,tsx}", 
   ],
   theme: {
-    extend: {},
+    extend: {
+		fontFamily: {
+			custom: ['"Text Me One"', 'sans-serif'],
+			schoolbell: ['Schoolbell', 'cursive'],
+			abel: ['Abel', 'sans-serif'],
+			baloo: ['"Baloo Thambi"', 'cursive'],
+			nunito: ['Nunito', 'sans-serif'],
+		},
+		colors: {
+			qumir: '#59CB07',
+			caramel: '#89E219',
+			futuro: '#4B4B4B',
+			riti: '#FFFFFF',
+			azul_cool: '#1EAFF6',
+			yawar: '#FF4B4B',
+			mostaza: '#FFC803',
+			pinki: '#CE82FF',
+			titulo: '#59CB07',
+			texto: '#2A2E25',
+			texto2: '#BBBDB9',
+			resaltar: '#07B7CB',
+		},
+		// otras configuraciones
+	},
   },
   plugins: [],
 }
