7cc3b832bdf9ccb220d52925e6c271d2298fb4a0
Tania Choque <taniachque@gmail.com>
2025-04-19 10:38:08 -0400

    Updated anagram game without timer.

--- src/components/juegos/Anagram.jsx
+++ src/components/juegos/Anagram.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect, useCallback } from 'react';
 import GameOver from './GameOver';
 import wordList from './WordList';
-import fondo_simi from '../assets/images/fondo_simi2.png';
 
 function Anagram() {
   const [currentWord, setCurrentWord] = useState('');
@@ -47,55 +46,61 @@ function Anagram() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-8"
-    style={{ backgroundImage: `url(${fondo_simi.src})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+    <div className="flex flex-col items-center justify-center min-h-screen p-8">
       <div className="text-center">
-        <h1 className='text-3xl font-bold mb-8'>Anagrama - T’ikraspa pukllay</h1>
-        <p className="text-sm mb-8">Kaypi t’ukunayki kay sananpakunamanta tikraspa t’ikraspa allin simikunata tarinaykikama</p>
-        <p className="text-sm mb-4">Desafía tu mente reorganizando letras para formar palabras. Encuentra todas las combinaciones posibles antes de que el tiempo se agote.</p>
-        <div className="border rounded p-12 bg-[#bef789] mb-8">
-          <label htmlFor="category" className="block mb-4 text-base font-bold">Selecciona un tema:</label>
-          <select
-            id="category"
-            value={selectedCategory}
-            onChange={(e) => setSelectedCategory(e.target.value)}
-            className="border border-gray-300 p-2 rounded w-64"
-          >
-            {Object.keys(wordList).map(category => (
-              <option key={category} value={category}>
-                {category.charAt(0).toUpperCase() + category.slice(1)}
-              </option>
-            ))}
-          </select>
-
-        <h2 className="text-xl font-bold mt-4 mb-4">Reordena las letras:</h2>
-        {shuffledWord && !isGameOver && (
-          <div className='flex flex-col items-center'>
-          <p className="text-4xl mb-4">{shuffledWord}</p>
-          <div className='flex items-center mb-4'>
-            <input
-              type="text"
-              className="border border-gray-300 p-2 rounded w-64 mr-2"
-              value={guess}
-              onChange={(e) => setGuess(e.target.value)}
-              placeholder="Escribe tu respuesta"
-            />
-            <button
-              onClick={checkGuess}
-              className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
+      <h1 className='text-3xl font-bold mb-0 text-titulo'>Anagrama</h1>
+        <h1 className='text-2xl font-bold text-futuro mb-4'>T’ikraspa pukllay</h1>
+        <p className="text-sm mb-4">Reordena las letras y forma tantas palabras como puedas antes que se acabe el tiempo.</p>
+        <div className="flex flex-col items-center justify-center">
+          <div className="border rounded-xl p-8 w-96 bg-qumir bg-opacity-20 mb-8 text-left items-center justify-center shadow-lg">
+            <label htmlFor="category" className="blockmb-4 text-base font-bold">Elige un tema:</label>
+            <select
+              id="category"
+              value={selectedCategory}
+              onChange={(e) => setSelectedCategory(e.target.value)}
+              className="bg-riti border border-gray-300 p-2 rounded-lg w-72 shadow-lg mt-4"
             >
-              Comprobar
-            </button>
+              {Object.keys(wordList).map(category => (
+                <option key={category} value={category}>
+                  {category.charAt(0).toUpperCase() + category.slice(1)}
+                </option>
+              ))}
+            </select>
           </div>
-          <button
-                onClick={changeWord}
-                className="bg-green-500 text-white p-2 rounded hover:bg-green-600 mb-4"
-              >
-                Cambiar Palabra
-              </button>
-        </div>
-        )}
-        </div>
+        </div>  
+        {/* <h2 className="text-xl font-bold mt-4 mb-4">Reordena las letras:</h2>*/}
+          {shuffledWord && !isGameOver && (
+              <div className='flex flex-col items-center'>
+                  <p className="text-6xl font-extrabold mb-10 text-titulo tracking-[0.25em] border border-titulo p-4 rounded-lg shadow-lg w-full max-w-lg shadow-green">
+                      {shuffledWord}
+                  </p>
+                  <div className='flex items-center mb-16 w-full max-w-lg'>
+                      <input
+                          type="text"
+                          className="border border-qumir p-4 rounded-lg w-full max-w-lg shadow-lg shadow-green text-center text-3xl font-extrabold tracking-wide shadow-green-100"
+                          value={guess}
+                          onChange={(e) => setGuess(e.target.value)}
+                          // placeholder="Escribe tu respuesta"
+                      />
+                  </div>
+                  <div className='flex mb-12 justify-between'>
+                      <button
+                          onClick={changeWord}
+                          className="bg-yawar text-white font-extrabold p-4 rounded-lg hover:bg-red-600 w-48 mr-12"
+                      > <i className="fa fa-chevron-right ml-1 mr-2 text-xl"></i>
+                          Cambiar Palabra
+                      </button>
+                      <button
+                          onClick={checkGuess}
+                          className="bg-azul_cool text-white font-extrabold p-4 rounded-lg hover:bg-blue-600 w-48 "
+                      >
+                          Comprobar <i className="fa fa-check-circle ml-1 mr-2 text-xl"></i>
+                      </button>
+                  </div>
+              </div>
+          )}
+
+
         {/* Mostrar el resultado */}
         {isGameOver && (
           <GameOver
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,19 +3,11 @@ export default {
 	content: [
 		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
 		'!./src/pages/index.astro',
-		'!./src/components/Header.astro',
-		'!./src/components/Footer.astro',
+		//'!./src/components/Header.astro',
+		//'!./src/components/Footer.astro',
 	],
 	theme: {
-		extend: {
-			fontFamily: {
-				custom: ['"Text Me One"', 'sans-serif'],
-				schoolbell: ['Schoolbell', 'cursive'],
-				abel: ['Abel', 'sans-serif'],
-				baloo: ['"Baloo Thambi"', 'cursive'],
-				nunito: ['Nunito', 'sans-serif'],
-			  },
-		},
+		extend: {},
 	},
 	corePlugins: {
 		container: false,
