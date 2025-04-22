58c3d4666bd11ddd9f56c9168e1cf614ecd75375
Tania Choque <taniachque@gmail.com>
2025-04-22 12:04:00 -0400

    Modified responsivenes in anagaram game.

--- src/components/juegos/Anagram.jsx
+++ src/components/juegos/Anagram.jsx
@@ -60,19 +60,19 @@ function Anagram() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-8">
+    <div className="flex flex-col items-center justify-center min-h-screen p-4 md:p-8 mx-4">
       <div className="text-center">
-      <h1 className='text-3xl font-bold mb-0 text-titulo'>Anagrama</h1>
+        <h1 className='text-3xl font-bold mb-0 text-titulo'>Anagrama</h1>
         <h1 className='text-2xl font-bold text-futuro mb-4'>T’ikraspa pukllay</h1>
         <p className="text-sm mb-4">Reordena las letras y forma tantas palabras como puedas antes que se acabe el tiempo.</p>
-        <div className="flex flex-col items-center justify-center">
-          <div className="border rounded-xl p-8 w-96 bg-qumir bg-opacity-20 mb-8 text-left items-center justify-center shadow-lg">
+        <div className="flex flex-col items-center justify-center w-full">
+          <div className="border rounded-xl p-4 md:p-8 w-96 bg-qumir bg-opacity-20 mb-8 text-left shadow-lg">
             <label htmlFor="category" className="block mb-4 text-base font-bold">Elige un tema:</label>
             <select
               id="category"
               value={selectedCategory}
               onChange={(e) => setSelectedCategory(e.target.value)}
-              className="bg-riti border border-gray-300 p-2 rounded-lg w-80 shadow-lg mt-4"
+              className="bg-riti border border-gray-300 p-2 rounded-lg w-full shadow-lg mt-4"
             >
               {Object.keys(wordList).map(category => (
                 <option key={category} value={category}>
@@ -86,39 +86,38 @@ function Anagram() {
             </div>
           </div>
         </div>  
-        {/* <h2 className="text-xl font-bold mt-4 mb-4">Reordena las letras:</h2>*/}
-          {shuffledWord && !isGameOver && (
-              <div className='flex flex-col items-center'>
-                  <p className="text-6xl font-extrabold mb-10 text-titulo tracking-[0.25em] border border-titulo p-4 rounded-lg shadow-lg w-full max-w-lg shadow-green">
-                      {shuffledWord}
-                  </p>
-                  <div className='flex items-center mb-16 w-full max-w-lg'>
-                      <input
-                          type="text"
-                          className="border border-qumir p-4 rounded-lg w-full max-w-lg shadow-lg shadow-green text-center text-3xl font-extrabold tracking-wide shadow-green-100"
-                          value={guess}
-                          onChange={(e) => setGuess(e.target.value)}
-                          // placeholder="Escribe tu respuesta"
-                      />
-                  </div>
-                  <div className='flex mb-12 justify-between'>
-                      <button
-                          onClick={changeWord}
-                          className="bg-yawar text-white font-extrabold p-4 rounded-lg hover:bg-red-600 w-48 mr-12"
-                      > <i className="fa fa-chevron-right ml-1 mr-2 text-xl"></i>
-                          Cambiar Palabra
-                      </button>
-                      <button
-                          onClick={checkGuess}
-                          className="bg-azul_cool text-white font-extrabold p-4 rounded-lg hover:bg-blue-600 w-48 "
-                      >
-                          Comprobar <i className="fa fa-check-circle ml-1 mr-2 text-xl"></i>
-                      </button>
-                  </div>
-              </div>
-          )}
+        
+        {shuffledWord && !isGameOver && (
+          <div className='flex flex-col items-center'>
+            <p className="text-6xl font-extrabold mb-10 text-titulo tracking-[0.25em] border border-titulo p-4 rounded-lg shadow-lg w-full max-w-lg shadow-green mx-2">
+              {shuffledWord}
+            </p>
+            <div className='flex items-center mb-16 w-full max-w-lg'>
+              <input
+                type="text"
+                className="border border-qumir p-4 rounded-lg w-full shadow-lg text-center text-3xl font-extrabold tracking-wide shadow-green-100"
+                value={guess}
+                onChange={(e) => setGuess(e.target.value)}
+              />
+            </div>
+            <div className='flex mb-12 justify-between w-full max-w-lg'>
+              <button
+                onClick={changeWord}
+                className="bg-yawar text-white font-extrabold p-4 rounded-lg hover:bg-red-600 w-48 mr-2"
+              >
+                <i className="fa fa-chevron-right ml-1 mr-2 text-xl"></i>
+                Cambiar Palabra
+              </button>
+              <button
+                onClick={checkGuess}
+                className="bg-azul_cool text-white font-extrabold p-4 rounded-lg hover:bg-blue-600 w-48 mr-2"
+              >
+                Comprobar <i className="fa fa-check-circle ml-1 mr-2 text-xl"></i>
+              </button>
+            </div>
+          </div>
+        )}
 
-        {/* Mostrar el resultado */}
         {isGameOver && (
           <GameOver
             isWinner={guess.toLowerCase() === currentWord}
