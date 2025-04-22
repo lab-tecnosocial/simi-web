e4248038516b05224048caeee1dcd8ee692c4fba
Tania Choque <taniachque@gmail.com>
2025-04-22 12:19:08 -0400

    Reviewed responviveness in peolpesection done.

--- src/components/PeopleSection.jsx
+++ src/components/PeopleSection.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 const PeopleSection = () => {
-return (
-<section id="people-images" className="w-screen">
-<img
-src="/assets/images/presentacion.jpg"
-className="w-screen h-auto object-cover"
-alt="Personajes Simi"
-/>
-</section>
-);
+  return (
+    <section id="people-images" className="w-full">
+      <img
+        src="/assets/images/presentacion.jpg"
+        className="w-full h-auto object-cover"
+        alt="Personajes Simi"
+      />
+    </section>
+  );
 };
 
 export default PeopleSection;
