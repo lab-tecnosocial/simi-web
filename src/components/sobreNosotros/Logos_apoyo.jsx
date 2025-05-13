c7816ddeb0134563f02dc6675e60596597d85277
Tania Choque <taniachque@gmail.com>
2025-05-13 12:27:53 -0400

    Added logos of contributors, fixed navbar, replaced image for questionbox.

---
+++ src/components/sobreNosotros/Logos_apoyo.jsx
@@ -0,0 +1,25 @@
+// src/components/sobreNosotros/Logos.jsx
+import React from 'react';
+
+const Logos = () => {
+  return (
+    <div className="p-8">
+      <h2 className="text-4xl font-medium mb-20 text-center">Una iniciativa de:</h2>
+      <div className="grid grid-cols-2  md:grid-cols-2 mb-24">
+        <div className="flex justify-center">
+          <img src="/assets/images/logolabtecnosocial.png" alt="logolabtecnosocial" className="h-24 w-auto" />
+        </div>
+        <div className="flex justify-center">
+          <img src="/assets/images/logoatuqyachachiq.png" alt="logoatuqyachachiq" className="h-32" />
+        </div>
+      </div>
+
+      <h2 className="text-4xl font-medium mb-20 text-center">Colaboradores</h2>
+        <div className="flex justify-center mb-18">
+          <img src="/assets/images/quichwa_nina.jpg" alt="qhichwa" className="h-18" />
+        </div>
+    </div>
+  );
+};
+
+export default Logos;
