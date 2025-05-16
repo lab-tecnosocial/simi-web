f14013f555b82878658be846dd752706910687b6
Tania Choque <taniachque@gmail.com>
2025-05-16 12:19:12 -0400

    Fixing juego de mesa component but needs work.

---
+++ src/components/simiwanpullkana/cardsSection.jsx
@@ -0,0 +1,59 @@
+//import React from 'react';
+
+const cardsData = [
+    {
+        title: "Ilustración",
+        description: "Te ofrecemos servicios de ilustración para tu proyecto teniendo en cuenta la pertinencia cultural",
+        image: "/assets/images/juego/zorrito.png",
+    },
+    {
+        title: "Talleres",
+        description: "Realizamos talleres de iniciación en la ilustración y escritura de cuentos y comics en diferentes lenguas indígenas",
+        image: "/assets/images/juego/zorrito.png",
+    },
+    {
+        title: "Traducción",
+        description: "Te ayudamos en la traducción y edición de textos BILINGÛES (Quechua-Castellano)",
+        image: "/assets/images/juego/zorrito.png",
+    },
+];
+
+const cardsSection = () => {
+  return (
+    <section className="py-8 my-8 bg-texto2 bg-opacity-25 p-8 rounded-lg text-center">
+      <div className="py-8 px-10">
+        <div className="container mx-auto">
+          <h2 className="text-3xl text-black mb-4">Nuestros Servicios</h2>
+
+          <p className="text-xl text-black mb-6">
+            Si bien realizamos <b>material gratuito didáctico y literario en quechua</b>, también ofrecemos nuestros 
+            servicios de manera que Atuq Yachachiq sea una iniciativa autosustentable y podamos seguir liberando material. 
+            Atuq Yachachiq desafía las estructuras tradicionales de distribución del conocimiento que perpetúan sistemas de 
+            exclusión, las cuales refuerzan un modelo de inequidad, donde el acceso a libros y materiales culturales queda 
+            restringido a sectores privilegiados.  
+            <b>Cuando adquieres nuestros servicios también apoyas nuestro proyecto editorial que promueve el acceso equitativo 
+            de material literario en quechua y otras lenguas indígenas de Bolivia.</b>
+          </p>
+        </div>
+
+        <div className="container mx-auto mt-16">
+            <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-8">
+                {cardsData.map((card, index) => (
+                    <div key={index} className="bg-[#FFC803]/20 rounded-3xl h-80 sm:aspect-[5/4] mx-auto">
+                        <div className="flex items-center justify-center w-full text-center mt-10">
+                          <img src={card.image} alt={card.title} className="h-12" />
+                        </div>
+                        <div className="w-full text-center">
+                          <h2 className="text-2xl text-center text-[#B78E4C] font-nunito font-bold p-4">{card.title}</h2>
+                        </div>
+                        <p className="text-gray-600 text-xl text-center font-light p-4">{card.description}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default cardsSection;
--- src/components/simiwanpullkana/logoSection.jsx
+++ src/components/simiwanpullkana/logoSection.jsx
@@ -2,47 +2,47 @@ import atuq from '../assets/images/juego/colabs/atuq.jpg';
 
 const LogoSection = () => {
     return (
-        <section className="py-8">
-            <div className="grid grid-cols-2 gap-4 justify-items-center">
+        <section className="py-8 my-8">
+            <div className="grid grid-cols-1 gap-4 justify-items-center">
                 {/* Logo 1 con dos logotipos juntos */}
                 <div className="flex flex-col items-center">
-                    <h3 className='text-xl mb-2'>“ Simiwan Pukllana” es una iniciativa de:</h3>
-                    <div className="flex justify-center space-x-2">
-                        <img src="/assets/images/juego/colabs/atuq.jpg" alt="simi" className="h-20" />
+                    <h3 className='text-xl mb-2'>“Simiwan Pukllana” es una iniciativa de:</h3>
+                    <div className="flex justify-center space-x-2 pt-4">
+                        <img src="/assets/images/juego/atuq_logo.png" alt="simi" className="h-48 mr-4" />
                         <a href="https://simiquechua.app/" target="_blank" rel="">
-                            <img src="/assets/images/juego/colabs/simi.png" alt="simi" className="h-20" />
+                            <img src="/assets/images/logo_simi2025.png" alt="simi" className="h-48 ml-4" />
                         </a>
                     </div>
                 </div>
 
-                {/* Logo 2 con un solo logotipo */}
-                <div className="flex flex-col items-center">
-                    <h3 className='text-xl mb-2'>Con el apoyo financiero de:</h3>
-                    <div className="flex justify-center space-x-2">
-                        <a href="https://www.misereor.org/" target="_blank" rel="">
-                            <img src="/assets/images/juego/colabs/misereor.jpg" alt="misereor" className="h-20" />
+                {/* Logo 4 con tres solo logotipo */}
+                <div className="flex flex-col items-center mt-8">
+                    <h3 className='text-xl mb-2'>En coordinación con:</h3>
+                    <div className="flex justify-center space-x-2 pt-8">
+                        <a href="https://www.cedib.org/" target="_blank" rel="" className="place-content-center">
+                            <img src="/assets/images/juego/colabs/ced.png" alt="ced" className="h-56 mr-8" />
+                        </a>
+                        <a href="https://martadero.org" target="_blank" rel="" className="place-content-center">
+                            <img src="/assets/images/juego/colabs/martadero.jpg" alt="martadero" className="h-44" />
+                        </a>
+                        <a href="https://martadero.org/micros-para-avanzar/" target="_blank" rel="" className="place-content-center">
+                            <img src="/assets/images/juego/colabs/micros.jpg" alt="micros" className="h-32 ml-8" />
                         </a>
                     </div>
                 </div>
 
                 {/* Logo 3 con un solo logotipo */}
-                <div className="flex flex-col items-center">
-                    <h3 className='text-xl mb-2'>Gestiona:</h3>
-                        <img src="/assets/images/juego/colabs/fundimagen.jpg" alt="fundimagen" className="h-20" />
+                <div className="flex flex-col items-center mt-12">
+                    <h3 className='text-xl mb-2'>Gestionado por:</h3>
+                        <img src="/assets/images/juego/colabs/fundimagen.jpg" alt="fundimagen" className="h-36 mt-4" />
                 </div>
 
-                {/* Logo 4 con tres solo logotipo */}
-                <div className="flex flex-col items-center">
-                    <h3 className='text-xl mb-2'>En coordinación:</h3>
+                {/* Logo 2 con un solo logotipo */}
+                <div className="flex flex-col items-center mt-12">
+                    <h3 className='text-xl mb-2'>Con el apoyo financiero de:</h3>
                     <div className="flex justify-center space-x-2">
-                        <a href="https://www.cedib.org/" target="_blank" rel="">
-                            <img src="/assets/images/juego/colabs/ced.png" alt="ced" className="h-20" />
-                        </a>
-                        <a href="https://martadero.org" target="_blank" rel="">
-                            <img src="/assets/images/juego/colabs/martadero.jpg" alt="martadero" className="h-20" />
-                        </a>
-                        <a href="https://martadero.org/micros-para-avanzar/" target="_blank" rel="">
-                            <img src="/assets/images/juego/colabs/micros.jpg" alt="micros" className="h-20" />
+                        <a href="https://www.misereor.org/" target="_blank" rel="">
+                            <img src="/assets/images/juego/colabs/misereor.jpg" alt="misereor" className="h-36 mt-4" />
                         </a>
                     </div>
                 </div>
