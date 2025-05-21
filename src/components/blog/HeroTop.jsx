c6cf010991bd9c080b9180012399518ef76d4e8f
Tania Choque <taniachque@gmail.com>
2025-05-21 10:11:07 -0400

    Fixed blog component.

--- src/components/blog/HeroTop.jsx
+++ src/components/blog/HeroTop.jsx
@@ -10,7 +10,7 @@ const HeroTop = () => {
   
           {/*<!-- Text Container (Positioned Left) -->*/}
           <div class="absolute top-[60%] left-10 transform -translate-y-[60%] text-white max-w-lg ml-24">
-            <h1 class="text-4xl font-bold">Descubre ideas, inspircación y consejos en nuestro blog.<br />¡Explora y sorpréndete!</h1>
+            <h1 class="text-4xl font-bold">Descubre ideas, inspiración y consejos en nuestro blog.<br />¡Explora y sorpréndete!</h1>
           </div>
         </div>  
       </div>
