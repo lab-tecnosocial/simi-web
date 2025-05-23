61cda0633d66727a8f6aea8c95fd0f2a44746739
Tania Choque <taniachque@gmail.com>
2025-05-23 12:55:38 -0400

    Fixed styles in blog

--- src/components/blog/post/Content.jsx
+++ src/components/blog/post/Content.jsx
@@ -1,6 +1,6 @@
 const Content = ({ content }) => {
     return (
-      <section className="container mx-auto px-4 mt-10 mb-20">
+      <section className="container mx-auto px-20 mt-10 mb-20">
         <div className="prose prose-lg max-w-none text-[#4B4B4B] text-xl font-nunito" dangerouslySetInnerHTML={{ __html: content }} />
       </section>
     );
