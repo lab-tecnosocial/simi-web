fd22b870a9b1e72b28b6e64aa5e0e7ac3e163ba5
Tania Choque <taniachque@gmail.com>
2025-05-23 11:26:54 -0400

    Fixed styles in anagram game.

--- src/components/juegos/Anagram.jsx
+++ src/components/juegos/Anagram.jsx
@@ -66,13 +66,13 @@ function Anagram() {
         <h1 className='text-2xl font-bold text-futuro mb-4'>T’ikraspa pukllay</h1>
         <p className="text-sm mb-4">Reordena las letras y forma tantas palabras como puedas antes que se acabe el tiempo.</p>
         <div className="flex flex-col items-center justify-center w-full">
-          <div className="border rounded-xl p-4 md:p-8 w-96 bg-qumir bg-opacity-20 mb-8 text-left shadow-lg">
-            <label htmlFor="category" className="block mb-4 text-base font-bold">Elige un tema:</label>
+          <div className="border rounded-xl p-4 md:p-8 w-96 bg-qumir bg-opacity-20 mb-8 text-left shadow-xl">
+            <label htmlFor="category" className="block mb-2 text-base font-bold">Elige un tema:</label>
             <select
               id="category"
               value={selectedCategory}
               onChange={(e) => setSelectedCategory(e.target.value)}
-              className="bg-riti border border-gray-300 p-2 rounded-lg w-full shadow-lg mt-4"
+              className="bg-riti border border-gray-300 p-4 rounded-lg w-full shadow-lg mt-4 text-lg font-extrabold"
             >
               {Object.keys(wordList).map(category => (
                 <option key={category} value={category}>
@@ -82,14 +82,14 @@ function Anagram() {
             </select>
             <div className="flex justify-between items-center mt-4">
               <p className="font-bold">Tiempo:</p>
-              <p className="bg-riti border border-gray-300 p-2 rounded-lg w-24 text-center shadow-lg text-xl font-black">{Math.floor(timeLeft / 60)}:{(timeLeft % 60).toString().padStart(2, '0')}</p>
+              <p className="bg-riti border border-gray-300 p-2 rounded-lg w-24 text-center shadow-lg text-2xl font-extrabold flex-grow ml-2">{Math.floor(timeLeft / 60)}:{(timeLeft % 60).toString().padStart(2, '0')}</p>
             </div>
           </div>
         </div>  
         
         {shuffledWord && !isGameOver && (
           <div className='flex flex-col items-center'>
-            <p className="text-6xl font-extrabold mb-10 text-titulo tracking-[0.25em] border border-titulo p-4 rounded-lg shadow-lg w-full max-w-lg shadow-green mx-2">
+            <p className="text-6xl font-extrabold mb-10 text-titulo tracking-[0.50em] border border-qumir p-4 rounded-lg shadow-lg w-full max-w-lg shadow-green mx-2">
               {shuffledWord}
             </p>
             <div className='flex items-center mb-16 w-full max-w-lg'>
@@ -103,16 +103,15 @@ function Anagram() {
             <div className='flex mb-12 justify-between w-full max-w-lg'>
               <button
                 onClick={changeWord}
-                className="bg-yawar text-white font-extrabold p-4 rounded-lg hover:bg-red-600 w-48 mr-2"
+                className="bg-yawar text-white font-extrabold p-4 rounded-lg hover:bg-red-600 w-48 flex-grow mr-2"
               >
-                <i className="fa fa-chevron-right ml-1 mr-2 text-xl"></i>
                 Cambiar Palabra
               </button>
               <button
                 onClick={checkGuess}
-                className="bg-azul_cool text-white font-extrabold p-4 rounded-lg hover:bg-blue-600 w-48 mr-2"
+                className="bg-azul_cool text-white font-extrabold p-4 rounded-lg hover:bg-blue-600 flex-grow w-48 mr-2"
               >
-                Comprobar <i className="fa fa-check-circle ml-1 mr-2 text-xl"></i>
+                Comprobar
               </button>
             </div>
           </div>
