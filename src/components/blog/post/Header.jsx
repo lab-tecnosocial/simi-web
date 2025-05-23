897a69879ff21ef2bf9bc84656412e55b1856866
Tania Choque <taniachque@gmail.com>
2025-05-23 18:18:32 -0400

    Fixed blog content, padding and margin but needs work for traslation.

--- src/components/blog/post/Header.jsx
+++ src/components/blog/post/Header.jsx
@@ -1,9 +1,10 @@
 const Header = ({ post }) => {
     const img_path = '../' + post.image;
     return (
-      <section className="container mx-auto px-4 mt-16 mb-10">
+      <section className="container mx-auto px-16 mt-2 mb-10">
         <h1 className="text-4xl font-bold text-black font-nunito">{post.title}</h1>
-        <p className="text-2xl text-[#5B5959] mb-4 font-nunito">Escrito por: {post.author}</p>
+        <p className="text-2xl text-[#5B5959]  mt-4 mb-4 font-nunito">Versión en español</p>
+        <p className="text-2xl text-[#5B5959]  mt-4 mb-4 font-nunito">Escrito por: {post.author}</p>
   
         <div className="w-full max-w-screen-lg mx-auto">
           <div className="relative w-full aspect-[5/3] overflow-hidden rounded-lg">
