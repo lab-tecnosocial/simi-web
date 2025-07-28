77a1d188936ccc80af5d7bc6f961382ef95007c8
Tania Choque <taniachque@gmail.com>
2025-07-27 20:45:55 -0400

    Fixed link to redirect to playstore and hero1 responsive in index.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,12 @@
 const Hero = () => {
   return (
-    <section className="relative flex justify-center items-center min-h-screen p-10 overflow-hidden">
+    <section className="relative flex justify-center items-start xl:items-center h-auto xl:min-h-screen p-10 overflow-hidden">
 
       <div
         className="absolute inset-0 bg-cover bg-center bg-no-repeat opacity-20 z-0"
       ></div>
 
-      <div className="relative z-10 flex flex-col md:flex-row items-center gap-6 transform origin-center scale-[1.4] md:scale-[1.6]">
+      <div className="relative z-10 flex flex-col xl:flex-row items-center gap-6 transform origin-center scale-100 xl:scale-[1.2] 2xl:scale-[1.4]">
         <img
           src="/assets/images/QHICHWAPI PARLANA.png"
           alt="Personaje aprendiendo"
@@ -21,9 +21,15 @@ const Hero = () => {
           </h1>
 
           <div className="flex flex-col gap-3 mt-4">
-            <button className="bg-[#58CB05] text-white font-normal py-2 px-6 rounded-2xl shadow-[0_6px_12px_rgba(0,0,0,0.2)] hover:bg-green-700 transition-all w-48 transform hover:scale-105">
-              DESCARGAR APP
-            </button>
+            <a
+              href="https://play.google.com/store/apps/details?id=org.labtecnosocial.simi"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="bg-[#58CB05] text-white font-normal py-2 px-6 rounded-2xl shadow-[0_6px_12px_rgba(0,0,0,0.2)] hover:bg-green-700 transition-all w-48 transform hover:scale-105">
+                DESCARGAR APP
+              </button>
+            </a>
           </div>
         </div>
       </div>
